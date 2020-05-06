7616e88743a56a83f40ddd05c3d29bb20161b5cf
Edrean Ernst <edrean@allesbeste.com>
2020-05-07 01:00:40 +0200

    Fix region dropdown issue Correct ArtifactUploader test with new somefunc index.js file hash

--- src/main/java/com/amazonaws/jenkins/plugins/sam/util/BeanHelper.java
+++ src/main/java/com/amazonaws/jenkins/plugins/sam/util/BeanHelper.java
@@ -9,6 +9,9 @@ import com.amazonaws.regions.Regions;
 
 import hudson.util.ListBoxModel;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * @author Trek10, Inc.
  */
@@ -17,10 +20,16 @@ public class BeanHelper {
     public static ListBoxModel doFillRegionItems() {
         ListBoxModel list = new ListBoxModel();
         for (Region region : RegionUtils.getRegions()) {
-            Regions regionData = Regions.fromName(region.getName());
-            list.add(regionData.getDescription(), regionData.getName());
+            String regionName = region.getName();
+            try {
+                Regions regionData = Regions.fromName(regionName);
+                list.add(regionData.getDescription(), regionData.getName());
+            } catch(Exception e) {
+                LOGGER.log(Level.INFO, "Failed to enumerate AWS region '" + regionName + "'", e);
+            }
         }
         return list;
     }
 
+    private static final Logger LOGGER = Logger.getLogger(BeanHelper.class.getName());
 }
